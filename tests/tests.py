b9ba4dcd033b224e5dc9dafcbc1ecdc2d8a3076f
Dave Greenwood <dgrnwd@gmail.com>
2021-01-06 17:40:25 +0000

    testing

--- tests/tests.py
+++ tests/tests.py
@@ -7,6 +7,31 @@ from dskeleton.skeleton import Skeleton
 from dskeleton.geometry import batch_vector_rotation
 
 
+ZR = torch.eye(3)[None, ...].repeat(4, 1, 1)
+
+R = torch.tensor([
+    [[1, 0, 0],
+     [0, 1, 0],
+     [0, 0, 1]],
+    [[0.707, 0.707, 0, 0],
+     [-0.707, -0.707, 0],
+     [0, 0, 1]],
+    [[0, -1, 0],
+     [1, 0, 0],
+     [0, 0, 1]],
+    [[0.707, -0.707, 0],
+     [0.707, 0.707, 0],
+     [0, 0, 1]]
+])
+
+X = torch.tensor([
+    [0, 0, 0],
+    [0.707, 0.707, 0],
+    [0, 1.414, 0],
+    [1.707, 0.707, 0]
+])
+
+
 class SkeletonTests(unittest.TestCase):
     """Test dskeleton."""
     pts = torch.tensor(np.load("tests/pts100.npy"))
@@ -34,6 +59,12 @@ class SkeletonTests(unittest.TestCase):
         self.assertGreater(skel.lengths.sum(), k)
         self.assertTrue((skel.lengths > 0).all())
 
+    def test_rotation2points1(self):
+        """Test that identity does not apply rotation."""
+        skel = Skeleton("TEST")
+        xyz = skel._rotation2points(ZR[None, ...])
+        self.assertTrue(torch.allclose(skel.xyz[0], xyz))
+
 
 class GeometryTests(unittest.TestCase):
 
@@ -53,3 +84,18 @@ class GeometryTests(unittest.TestCase):
         _a = r @ a[..., None]
         _b = b[..., None]
         self.assertTrue(torch.allclose(_a, _b))
+
+    def test_rotation3(self):
+        """Test two orthoganal vectors return 90 degree rotations."""
+        a = torch.tensor([[0, 1, 0.], [-1, 0, 0.]])
+        b = torch.tensor([[-1, 0, 0.], [0, 1, 0.]])
+
+        _r = torch.tensor([[[0., -1.,  0.],
+                            [1.,  0.,  0.],
+                            [0.,  0.,  1.]],
+                           [[0.,  1.,  0.],
+                            [-1.,  0.,  0.],
+                            [0.,  0.,  1.]]])
+
+        r = batch_vector_rotation(a, b)
+        self.assertTrue(torch.allclose(_r, r))
