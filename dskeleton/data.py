1df3d5f7667cec1a51a006b21628ce59ba917e03
Dave Greenwood <dgrnwd@gmail.com>
2021-01-10 16:57:29 +0000

    udh upper head only

--- dskeleton/data.py
+++ dskeleton/data.py
@@ -6,6 +6,8 @@ SKELETONS = dict(
         pkgutil.get_data(__name__, "data/test.json")),
     UDH_UPPER = json.loads(
         pkgutil.get_data(__name__, "data/udh_upper.json")),
+    UDH_HEAD = json.loads(
+        pkgutil.get_data(__name__, "data/udh_head.json")),
     BODY_25 = json.loads(
         pkgutil.get_data(__name__, "data/body_25.json"))
 )
