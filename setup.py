e4396909b8c160964283f78a09c660226327b1cf
Dave Greenwood <dgrnwd@gmail.com>
2021-01-14 09:47:50 +0000

    setup

---
+++ setup.py
@@ -0,0 +1,10 @@
+"""Basic setup for entry points."""
+from setuptools import setup
+
+setup(name='dskeleton',
+      version='0.1',
+      description='A Differentiable Skeleton.',
+      author='Dave Greenwood',
+      py_modules=["dskeleton"],
+      zip_safe=False
+      )
